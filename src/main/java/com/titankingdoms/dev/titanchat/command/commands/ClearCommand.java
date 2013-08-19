360c629d88865becee619c828a1d470b84eb809a
NodinChan <nodinchan@live.com>
2013-08-19 17:41:17 +0800

    No point in clearing the console and added clear scollback option

--- src/main/java/com/titankingdoms/dev/titanchat/command/commands/ClearCommand.java
+++ src/main/java/com/titankingdoms/dev/titanchat/command/commands/ClearCommand.java
@@ -17,8 +17,11 @@
 
 package com.titankingdoms.dev.titanchat.command.commands;
 
+import java.util.List;
+
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.titankingdoms.dev.titanchat.command.Command;
 import com.titankingdoms.dev.titanchat.util.Messaging;
@@ -28,12 +31,46 @@ public final class ClearCommand extends Command {
 	public ClearCommand() {
 		super("Clear");
 		setAliases("clr");
-		setArgumentRange(0, 0);
+		setArgumentRange(0, 1);
 		setDescription("Clear chat");
+		getLayer().registerAll(new Scrollback());
 	}
 	
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-		Messaging.sendMessage(sender, StringUtils.repeat("\n", 100));
+		if (!(sender instanceof Player)) {
+			Messaging.sendMessage(sender, "You cannot clear the console");
+			return;
+		}
+		
+		if (args.length < 1)
+			Messaging.sendMessage(sender, StringUtils.repeat("\n", 20));
+		else
+			getLayer().execute(sender, args);
+	}
+	
+	@Override
+	public List<String> tab(CommandSender sender, String[] args) {
+		return getLayer().tab(sender, args);
+	}
+	
+	public final class Scrollback extends Command {
+		
+		public Scrollback() {
+			super("Scrollback");
+			setAliases("s");
+			setArgumentRange(0, 0);
+			setDescription("Clear chat including scrollback");
+		}
+		
+		@Override
+		public void execute(CommandSender sender, String[] args) {
+			if (!(sender instanceof Player)) {
+				Messaging.sendMessage(sender, "You cannot clear the console");
+				return;
+			}
+			
+			Messaging.sendMessage(sender, StringUtils.repeat("\n", 100));
+		}
 	}
 }
